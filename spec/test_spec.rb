3d6fac74705291628193c9c258e38391ff52cb9b
uedayusuke <uedayusuke1107@gmail.com>
2020-07-22 09:41:52 +0000

    [EDIT]test_spec

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,7 +1,13 @@
 describe "四則演算" do
 	context "足し算" do
-		it "1 + 1 は 2 に な る"  do
+		it "1 + 1 は 2 になる"  do
 			expect(1 + 1).to eq 2
+		end
+	end
+
+	context "足し算" do
+		it "1 + 1 は 2 になる" do
+			expect(1 + 1).to eq 3
 		end
 	end
 end
