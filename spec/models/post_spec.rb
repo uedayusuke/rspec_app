0e6716a798ea6cd0ed49ab0481f2b72f1f9c2006
uedayusuke <uedayusuke1107@gmail.com>
2020-07-30 08:57:23 +0000

    [EDIT]post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-	pending "add some examples to (or delete) #{__FILE__}"
+	context "データが正しく保存される" do
+		before do
+			@post = Post.new
+			@post.title = "Hello WebCamp"
+			@post.content = "今日も晴れです。"
+			@post.save
+		end
+		it "全て入力してるので保存される" do
+			expect(@post).to be_vaild
+		end
+	end
 end
