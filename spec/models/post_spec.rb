81ecc1af768f86f80e5bffb535c905a4a19a6029
uedayusuke <uedayusuke1107@gmail.com>
2020-07-30 09:03:59 +0000

    [EDIT]post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,7 +9,20 @@ RSpec.describe Post, type: :model do
 			@post.save
 		end
 		it "全て入力してるので保存される" do
-			expect(@post).to be_vaild
+			expect(@post).to be_valid
 		end
 	end
+
+	context "データが正しく保存されない" do
+    before do
+      @post = Post.new
+      @post.title = ""
+      @post.content = "今日も晴れです。"
+      @post.save
+    end
+    it "titleが入力されていないので保存されない" do
+      expect(@post).to be_invalid
+      expect(@post.errors[:title]).to include("can't be blank")
+    end
+  end
 end
